9c76d198163ee9c7f3b62842e08a858ff32090fd
Ruth Dable <P2608522@my365.dmu.ac.uk>
2022-08-09 18:34:19 +0100

    added some more test data in cls Staff

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public int StaffId { get; set; }
+        public string StaffLastName { get; set; }
+        public string StaffDescription { get; set; }
+        public DateTime DateJoined { get; set; }
+        public decimal StaffSalary { get; set; }
+        public bool StaffAvailable { get; set; }
+        public string StaffPhoneNo { get; set; }
     }
 }
